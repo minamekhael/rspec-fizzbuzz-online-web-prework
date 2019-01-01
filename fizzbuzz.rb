86ca6dbb448a3f8ec8d34f7b43dc4230d71dd531
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-01 22:29:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
 
-if int % 3 == 0 
+if int % 15 == 0 
     puts  
-    "Fizz"
-elsif int % 15 == 0
-    puts 
     "FizzBuzz"
+elsif int % 3 == 0
+    puts 
+    "Fizz"
 elsif int % 5 == 0 
     puts 
     "Buzz"
