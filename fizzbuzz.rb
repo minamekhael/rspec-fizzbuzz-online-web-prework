89b9806bbf1b0f4530e2a16554d58070a146ae18
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-01 22:05:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@ if int % 3 && 5 == 0
     puts  
     "FizzBuzz"
 elsif 
-  int % (5) == 0 
+  int % 5 == 0 
     puts "Buzz"
  elsif 
-  int % (3) == 0 
+  int % 3 == 0 
     puts
     "Fizz"
   end
